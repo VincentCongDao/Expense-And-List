c78c1ceac5531e2fd6702dfe1b3dcb40c77539a0
VincentCongDao <congdanhdao95@gmail.com>
2024-08-14 13:57:06 -0700

    Adding hono to handle API especially with the middleware handling the API coming with the package of zod, and hono clerk.

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,29 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+import author from "./author";
+import book from "./book";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.route("/author", author);
+app.route("/book", book);
+
+export const GET = handle(app);
+export const POST = handle(app);
+
+// app.get("/hello", clerkMiddleware(), (c) => {
+//   const auth = getAuth(c);
+
+//   if (!auth?.userId) {
+//     return c.json({
+//       error: "User is not authorized",
+//     });
+//   }
+//   return c.json({
+//     message: "Hello Next.js!",
+//     userId: auth.userId,
+//   });
+// });
